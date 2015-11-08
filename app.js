768929c3e97776fb17e0361eb95bb070533e5ccf
ee7klt <kltah1@gmail.com>
2015-11-08 18:14:30 +0800

    async vs sync: app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err,db) {
+
+  if (err) throw err;
+
+  db.collection('things').findOne({}, function(err,doc) {
+    console.dir(doc);
+    db.close();
+  });
+
+  console.dir("Called findOne!");
+
+});
