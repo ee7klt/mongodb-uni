160b5a921c32e382a89752bc1b8ebcf4ddf64819
ee7klt <kltah1@gmail.com>
2015-11-11 08:57:44 +0800

    hello combined

--- hello_express/app.js
+++ hello_express/app.js
@@ -1,18 +1,30 @@
 var express = require('express'),
-  app = express();
-  cons = require('consolidate');
+  app = express(),
+  cons = require('consolidate'),
+  MongoClient = require('mongodb').MongoClient,
+  Server = require('mongodb').Server;
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 app.set('views', __dirname + "/views");
 
+var mongoclient = new MongoClient(new Server('localhost', 27017));
+
+var db = mongoclient.db('course');
+
 app.get('/', function(req, res) {
-  res.render('hello', {'name' : 'Swig'});
-})
+  db.collection('hello_mongo_express').findOne({}, function(err,doc){
+    if (err) throw err;
+    res.render('hello', doc);
+  });
+});
 
 app.get('*', function(req, res) {
   res.status(404).send('404. page not found');
 })
 
-app.listen(8080);
-console.log('express server started on port 8080');
+mongoclient.open(function(err, mongoclient) {
+  if (err) throw err;
+  app.listen(8080);
+  console.log('express server started on port 8080');
+})
